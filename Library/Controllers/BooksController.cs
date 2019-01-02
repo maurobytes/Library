6ace55a6c7da2d989c8a72a1e06f945a606531ae
Mauricio Contreras <m_contreras@outlook.cl>
2019-01-02 13:54:31 -0300

    Partially updated Book with Patch

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using Library.API.Entities;
 using Library.API.Models;
 using Library.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -122,6 +123,35 @@ namespace Library.API.Controllers
             return NoContent();
         }
 
+        [HttpPatch("{id}")]
+        public IActionResult PartiallyUpdateBookForAuthor(Guid authorId, Guid id,
+            [FromBody] JsonPatchDocument<BookForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+                return BadRequest();
 
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
+
+            if (bookForAuthorFromRepo == null)
+                return NotFound();
+
+            var bookToPatch = Mapper.Map<BookForUpdateDto>(bookForAuthorFromRepo);
+
+            patchDoc.ApplyTo(bookToPatch);
+
+            //add validation
+
+            Mapper.Map(bookToPatch, bookForAuthorFromRepo);
+
+            _libraryRepository.UpdateBookForAuthor(bookForAuthorFromRepo);
+
+            if (!_libraryRepository.Save())
+                throw new Exception($"Patching book {id} for author {authorId} failed on save.");
+
+            return NoContent();
+        }
     }
 }
--- Library/Startup.cs
+++ Library/Startup.cs
@@ -78,6 +78,8 @@ namespace Library.API
                 cfg.CreateMap<BookForCreationDto, Book>();
 
                 cfg.CreateMap<BookForUpdateDto, Book>();
+
+                cfg.CreateMap<Book, BookForUpdateDto>();
             });
 
             libraryContext.EnsureSeedDataForContext();
