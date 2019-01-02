aa82ede7d0318a15712e7fcc87f777afa490e5f0
Mauricio Contreras <m_contreras@outlook.cl>
2019-01-02 16:54:37 -0300

    Book input validation

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -53,6 +53,15 @@ namespace Library.API.Controllers
             if (book == null)
                 return BadRequest();
 
+            if(book.Description == book.Title)
+            {
+                ModelState.AddModelError(nameof(BookForCreationDto),
+                    "The provided description should be different from the title.");
+            }
+
+            if (!ModelState.IsValid)
+                return new UnprocessableEntityObjectResult(ModelState);
+
             if (!_libraryRepository.AuthorExists(authorId))
                 return NotFound();
 
